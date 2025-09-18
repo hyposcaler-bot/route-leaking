7947d8998819ad0bc604867ca633fd4619c749e8
Fanshaw Dennis <64549250+hyposcaler-bot@users.noreply.github.com>
2025-09-18 23:15:26 +0000

    vrf-lite setup

--- configs/clients/client5.sh
+++
@@ -1,6 +0,0 @@
-ip addr add 10.255.50.15/24 dev eth1
-
-ip route add 10.255.10.0/24 via 10.255.50.1 dev eth1
-ip route add 10.255.20.0/24 via 10.255.50.1 dev eth1
-ip route add 10.255.30.0/24 via 10.255.50.1 dev eth1
-ip route add 10.255.40.0/24 via 10.255.50.1 dev eth1
